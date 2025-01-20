fc1e4f00cd3f73568121c9634de9930419301006
kppantino <kppantino@student.apc.edu.ph>
2025-01-20 16:48:20 +0800

    v-model add

--- home/js/vue.js
+++ home/js/vue.js
@@ -8,3 +8,30 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      itemName: null,
+      itemNumber: null,
+      itemImportant: false,
+      shoppingList: [
+        { name: 'Tomatoes', number: 5, important: false }
+      ]
+    }
+  },
+  methods: {
+    addItem(){
+      let item = {
+        name: this.itemName,
+        number: this.itemNumber,
+        important: this.itemImportant
+      }
+      this.shoppingList.push(item)
+      this.itemName = null
+      this.itemNumber = null  
+      this.itemImportant = false
+    }
+  }
+})
+app2.mount('#app2')
