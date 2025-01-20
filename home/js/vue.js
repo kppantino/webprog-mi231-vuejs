bb83387491a1786946d7c1a42754a74d4ebe6a06
kppantino <kppantino@student.apc.edu.ph>
2025-01-20 12:51:30 +0800

    edit

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,10 +1,10 @@
-  const app = Vue.createApp({
-   data() {
+const app = Vue.createApp({
+  data() {
     return {
-     message: "Happy New Year WEBPROG MI231!",
-     vueClass: "pinkBG"
-    }
-   }
-  })
-  
-  app.mount('#app')
+      message: "Happy New Year WEBPROG MI231!",
+      vueClass: "pinkBG"
+    };
+  }
+});
+
+app.mount('#app');
