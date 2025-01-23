7edcfd5b2723e075cd7cc4edfa67745f0af242fc
kppantino <kppantino@student.apc.edu.ph>
2025-01-23 16:44:22 +0800

    css edit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Food</h1>
-  <food-itemv/>
+  <food-item/>
   <food-item2/>
   <food-item/>
   <food-item2/>
--- vue-project/src/components/FoodItem.vue
+++ vue-project/src/components/FoodItem.vue
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-.apple {
+.apples {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
